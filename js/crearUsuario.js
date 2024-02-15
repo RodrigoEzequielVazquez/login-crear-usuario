b78f3cce81725b1d6de865c82c2d5037454205f1
Rodrigo Vazquez <rodrigo_vazquez_99@hotmail.com>
2024-02-15 16:38:37 -0300

    login-crear-usuario

---
+++ js/crearUsuario.js
@@ -0,0 +1,10 @@
+const btnIniciarSesion = document.getElementById("iniciarCrear")
+const btnCrearUsuario = document.getElementById("crearUsuario")
+
+btnIniciarSesion.addEventListener("click",(e)=>{
+    window.location.href = "index.html"
+})
+
+btnCrearUsuario.addEventListener("click",(e)=>{
+    alert("Esto no tiene funcionalidad, simplemente es una maquetación.")
+})
---
+++ js/index.js
@@ -0,0 +1,10 @@
+const btnIniciarSesion = document.getElementById("iniciarInicio")
+const btnCrearUsuario = document.getElementById("crearInicio")
+
+btnCrearUsuario.addEventListener("click",(e)=>{
+    window.location.href = "crearUsuario.html"
+})
+
+btnIniciarSesion.addEventListener("click",(e)=>{
+    alert("Esto no tiene funcionalidad, simplemente es una maquetación.")
+})
